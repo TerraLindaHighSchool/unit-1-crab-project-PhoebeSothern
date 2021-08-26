f064854e5c71e1388f4ed99d99ad32dfff9eb5e9
Phoebe K Sothern <ps01877@srcs.loc>
2021-08-26 15:45:56 -0700

    Completed "moving around"

--- Crab.java
+++ Crab.java
@@ -2,12 +2,41 @@ import greenfoot.*;
 
 /**
  * This class defines a crab. Crabs live on the beach.
+ * @author Phoebe Sothern
+ * @version 8/26/21
  */
+
 public class Crab extends Actor
 {
+    // This method repeats the following actions
     public void act()
     {
-        // Add your action code here
+        move(3);
+        turnAtEdge();
+        checkKeyPress();
+        onCollision();
+    }
+    
+    // Turns the Crab at the edge
+    private void turnAtEdge()
+    {
+
+    }
+    
+    
+    // Checks for user key presses so user can turn the Crab
+    private void checkKeyPress()
+    {
+        
+    }
+    
+    // Check for collisions with other objects
+    private void onCollision()
+    {
+       if(isAtEdge())
+       {
+           turn(-50);
+       }
     }
 }
 
